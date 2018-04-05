b4d863f202a7990dcbd14c21fca8a752e5778f66
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2018-04-05 21:18:14 +0500

    Initial commit

---
+++ app/src/main/java/com/gayratrakhimov/multithreadingexamples/MultithreadingProblemExampleActivity.java
@@ -0,0 +1,33 @@
+package com.gayratrakhimov.multithreadingexamples;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.concurrent.TimeUnit;
+
+public class MultithreadingProblemExampleActivity extends AppCompatActivity {
+
+    // this example demonstrates threading problem
+    // this example is taken from openprofession.ru course
+
+    private static int mCount = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        for (int x = 0; x < 10; x++) {
+            new Thread(() -> {
+                for (int y = 0; y < 100; y++) {
+                    mCount++;
+                    System.out.println(mCount+" "+Thread.currentThread().getName());
+                    try{
+                        TimeUnit.MILLISECONDS.sleep(100);
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+    }
+}
